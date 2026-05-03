f53fbd28f97a711121697ba28aa064bde66b1ede
Aditya <adityasorathiya.4112@gmail.com>
2026-05-03 19:01:07 +0530

    Add home endpoint to return server status

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
+@app.get("/")
+def home():
+    return {"message": "Server running"}
+
+
 @app.get("/create")
 def create():
     return {"res": "This create endpoint is working!!"}
