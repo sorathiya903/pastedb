c4ff4b92694076abe8e89fcfb6a50d3017192c3c
Aditya <adityasorathiya.4112@gmail.com>
2026-05-03 19:24:58 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# Enable CORS so your JS can talk to the backend if they are on different ports
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # In production, replace "*" with your frontend URL
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class DATA(BaseModel):
+    name: str
+    age: int
 
 @app.get("/")
 def home():
@@ -11,3 +25,12 @@ def home():
 @app.get("/create")
 def create():
     return {"res": "This create endpoint is working!!"}
+
+
+@app.post("/test")
+async def test(data: DATA):
+    name = data.name
+    age= data.age
+    res = f"the user name is  {name} and the age is {age}."
+    return {"ans":res}
+    
