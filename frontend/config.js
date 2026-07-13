303b1cb95be6995cc328cd05f4343893e80dcee1
Aditya <adityasorathiya.4112@gmail.com>
2026-07-13 21:30:10 +0530

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -2,3 +2,13 @@ const API_URL = "https://pastedb-rw62.onrender.com"
 const WS_URL = API_URL.replace(/^http/, "ws");
 
 const debug=false
+
+
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-H8NDTL7KT9');
+
+
